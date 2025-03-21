9cc208a0b999ad322b301eb4a8ebc5691e9e8476
lachatonfou <maniakitten@mail.ru>
2025-03-21 22:51:12 +1000

    домашка 5. доделки

--- src/test/java/ui/seleniumDZ5.java
+++ src/test/java/ui/seleniumDZ5.java
@@ -2,11 +2,11 @@ package ui;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -61,9 +61,99 @@ public class seleniumDZ5 {
     void proverkaDisabledInput() throws InterruptedException {
 
         WebElement disabledInput = driver.findElement(By.xpath("//input[@class='form-control' and @placeholder='Disabled input']"));
-
-        String placeholderText = disabledInput.getAttribute("value");
+        String placeholderText = disabledInput.getAttribute("placeholder");
         Thread.sleep(1000);
         assertEquals("Disabled input", placeholderText);
+    }
+
+    @Test
+    void proverkaReadonlyInput() throws InterruptedException {
+
+        WebElement readonlyInput = driver.findElement(By.xpath("//input[@class='form-control' and @value='Readonly input']"));
+        String placeholderText = readonlyInput.getAttribute("value");
+        Thread.sleep(1000);
+        assertEquals("Readonly input", placeholderText);
+    }
+
+    @Test
+    void proverkaDropdownSelect() throws InterruptedException {
+
+        WebElement dropdownSelect = driver.findElement(By.className("form-select"));
+        Select dropdown = new Select(dropdownSelect);
+        dropdown.selectByVisibleText("Two");
+        WebElement selectedOption = dropdown.getFirstSelectedOption();
+        String selectedValue = selectedOption.getText();
+        Thread.sleep(1000);
+        assertEquals("Two", selectedValue);
+    }
+
+    @Test
+    @Tag("доделать")
+    void proverkaDropdownDatalist() throws InterruptedException {
+
+       WebElement dropdownDatalist = driver.findElement(By.xpath("//input[@list='my-options']"));
+       dropdownDatalist.sendKeys("New York");
+       Thread.sleep(3000);
+        // проверка из серии - ввели текст и рады. нужна нормальная
+    }
+
+    @Test
+    @Tag("доделать")
+    void proverkaFileInput() throws InterruptedException {
+
+        WebElement fileInput = driver.findElement(By.name("my-file"));
+        Thread.sleep(3000);
+        //тут вообще нет проверки. хз как оно работает
+    }
+
+    @Test
+    void proverkaCheckedCheckbox() throws InterruptedException {
+
+        WebElement checkedCheckbox = driver.findElement(By.id("my-check-1"));
+        checkedCheckbox.click();
+        Thread.sleep(1000);
+        boolean isChecked = checkedCheckbox.isSelected();
+        WebElement checkedCheckboxLabel = driver.findElement(By.xpath("//label[normalize-space(.)='Checked checkbox']"));
+        String checkedCheckboxText = checkedCheckboxLabel.getText();
+        assertEquals("Checked checkbox", checkedCheckboxText);
+        assertEquals(false, isChecked);
+    }
+
+    @Test
+    void proverkaDefaultCheckbox() throws InterruptedException {
+
+        WebElement defaultCheckbox = driver.findElement(By.id("my-check-2"));
+        defaultCheckbox.click();
+        Thread.sleep(1000);
+        boolean isChecked = defaultCheckbox.isSelected();
+        WebElement checkedCheckboxLabel = driver.findElement(By.xpath("//label[normalize-space(.)='Default checkbox']"));
+        String checkedCheckboxText = checkedCheckboxLabel.getText();
+        assertEquals("Default checkbox", checkedCheckboxText);
+        assertEquals(true, isChecked);
+    }
+
+    @Test
+    void proverkaCheckedRadio() throws InterruptedException {
+
+        WebElement checkedRadio = driver.findElement(By.id("my-radio-1"));
+        Thread.sleep(1000);
+        boolean isChecked = checkedRadio.isSelected();
+        WebElement checkedCheckboxLabel = driver.findElement(By.xpath("//label[normalize-space(.)='Checked radio']"));
+        String checkedCheckboxText = checkedCheckboxLabel.getText();
+        assertEquals("Checked radio", checkedCheckboxText);
+        assertEquals(true, isChecked);
+    }
+
+    @Test
+    void proverkaDefaultRadio() throws InterruptedException {
+
+        WebElement defaultRadio = driver.findElement(By.id("my-radio-2"));
+        defaultRadio.click();
+        Thread.sleep(1000);
+        boolean isSelected = defaultRadio.isSelected();
+        WebElement checkedCheckboxLabel = driver.findElement(By.xpath("//label[normalize-space(.)='Default radio']"));
+        String checkedCheckboxText = checkedCheckboxLabel.getText();
+        assertEquals("Default radio", checkedCheckboxText);
+        assertEquals(true, isSelected);
     }
 }
